10fa24d1f6c0f312f51eec95289ff19a2ef3f41e
pia-negandhi <pia.srivastava@cognitivematch.com>
2013-12-11 18:15:27 -0800

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Pia on 12/11/13.
+//  Copyright (c) 2013 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
